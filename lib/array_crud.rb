6219e9317c9fdbeee793613a28f1413f84d3453a
Shayla White <shayladwhite1@gmail.com>
2019-05-08 06:45:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def add_element_to_start_of_array(array)
 
   my_array = Array.new
   my_array = [ "ginger", "spice", "everything", "nice"]
-my_array.unshift("arrays!")
+my_array.unshift(arraY)
  
 end
 
