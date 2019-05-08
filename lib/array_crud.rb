85ea83e131f4eec0b70904f4ed7ab390fa447384
Shayla White <shayladwhite1@gmail.com>
2019-05-08 06:17:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
  my_array = Array.new
  my_array = [ "ginger", "spice", "everything", "nice"]
  my_array.pop("arrays!")
-  
+ puts my_array.inspect
 end
 
 def remove_element_from_end_of_array(array)
