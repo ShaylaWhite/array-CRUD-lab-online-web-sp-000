4beb87fcdcf0597eb6478b6168e0632a1ed45fd3
Shayla White <shayladwhite1@gmail.com>
2019-05-08 07:16:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,21 +25,16 @@ end
 
 
 def remove_element_from_end_of_array(array)
-   my_array = Array.new
-  my_array = [ "ginger", "spice", "everything", "nice"]
+   
  my_array.pop(array)
  
- my_array.inspect
+
 puts my_array
 end
 
 
 def remove_element_from_start_of_array(array)
-   my_array = Array.new
-  my_array = [ "ginger", "spice", "everything", "nice"]
-my_array.shift(array)
-my_array.inspect
-puts my_array
+  
 end
 
 def retrieve_element_from_index(array, index_number)
