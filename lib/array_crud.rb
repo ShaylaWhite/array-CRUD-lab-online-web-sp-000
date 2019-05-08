ab0d7b55b1c335dd71cf94d2162f3ad566dd8784
Shayla White <shayladwhite1@gmail.com>
2019-05-08 06:32:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,18 +8,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- my_array = Array.new
- my_array = [ "ginger", "spice", "everything", "nice"]
- my_array.unshift("arrays!")
- puts my_array.inspect 
+
   
 end
 
 def add_element_to_start_of_array(array)
-my_array = Array.new
- my_array = [ "ginger", "spice", "everything", "nice"]
-door_array = my_array.pop
- puts my_array.inspect 
+
 
 end
 
