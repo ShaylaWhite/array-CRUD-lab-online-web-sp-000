dfe9fd863a3d836220a08b17013bc073d67bba50
Shayla White <shayladwhite1@gmail.com>
2019-05-08 07:21:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,51 +1,35 @@
-def create_an_empty_array
-  []
+ddef create_an_empty_array
+  arr=[]
 end
 
 def create_an_array
-  my_array = Array.new
-  my_array = [ "ginger", "spice", "everything", "nice"]
+  arr=[1,2,3,4,]
 end
 
 def add_element_to_end_of_array(array, element)
-   my_array = Array.new
-  my_array = [ "ginger", "spice", "everything", "nice"]
- my_array.push(element)
+  array.push(element)
 end
 
-
-def add_element_to_start_of_array(array,element)
- my_array = Array.new
-  my_array = [ "ginger", "spice", "everything", "nice"]
-
-my_array.unshift("wow")
- 
+def add_element_to_start_of_array(array, element)
+  array.unshift(element)
 end
-
-
 
 def remove_element_from_end_of_array(array)
-   
- arr.pop(array)
- 
-
-puts my_array
+  array.pop
 end
 
-
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_array[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  my_array[0]
+  array.first
 end
 
-
 def retrieve_last_element_from_array(array)
-    my_array[1]
+  array.last
 end
