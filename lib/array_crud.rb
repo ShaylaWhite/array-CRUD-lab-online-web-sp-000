efc8d4507f8416ea922bb2a5c70588c5664b2d97
Shayla White <shayladwhite1@gmail.com>
2019-05-08 06:15:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = Array.new
-my_array = [ "ginger", "spice", "everything", "nice"]
+ my_array = Array.new
+ my_array = [ "ginger", "spice", "everything", "nice"]
  my_array.unshift("arrays!")
   
 end
