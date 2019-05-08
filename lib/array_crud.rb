4d550f52480b1f88951d1148b151a16bdbb6d294
Shayla White <shayladwhite1@gmail.com>
2019-05-08 06:43:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   my_array = Array.new
+  my_array = [ "ginger", "spice", "everything", "nice"]
 my_array.push(array)
 end
 
