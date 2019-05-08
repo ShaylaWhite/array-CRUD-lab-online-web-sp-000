13d726801ebe3d11c65ed008682f5791451d4d2d
Shayla White <shayladwhite1@gmail.com>
2019-05-08 06:23:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_end_of_array(array, element)
   
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(array)
  my_array = Array.new
  my_array = [ "ginger", "spice", "everything", "nice"]
  my_array.pop("arrays!")
